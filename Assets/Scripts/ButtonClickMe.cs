ae3e590bfbee2bec071ea3184911143c963c8db2
Christian Sasso <christian.sasso@gmail.com>
2022-11-06 20:32:42 -0800

    Added Canvas with Click Me and Thank you buttons
    Added Canvas with Click Me and Thank you buttons.

---
+++ Assets/Scripts/ButtonClickMe.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+using Debug = UnityEngine.Debug;
+
+public class ButtonClickMe : MonoBehaviour
+{
+    public void Clicked()
+    {
+        Debug.Log("Click Me has been Clicked!");
+    }
+}
